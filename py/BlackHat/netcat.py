0f70f347fb43e7ca675376dce9613b867dfa0582
trainking <yefulincode@gmail.com>
2020-09-14 19:31:42 +0800

    add netcat.py

--- py/BlackHat/netcat.py
+++ py/BlackHat/netcat.py
@@ -34,13 +34,81 @@ def usage():
     sys.exit(0)
 
 def client_sender(buffer):
-    pass
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        clinet.connect((target, port))
+        if len(buffer):
+            client.send(buffer)
+        while True:
+            recv_len = 1
+            response = ""
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+                if recv_len < 4096:
+                    break;
+        print response
+    except :
+        print "[*] Exception!Exitiing."
+        client.close()
 
 def server_loop():
-    pass
+    global target
+
+    if not len(target):
+        target = "0.0.0.0"
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((target, port))
+
+        while True:
+            client_socket, addr = server.accept()
+            client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+            clien_thread.strart()
+
+def run_command(command):
+    command = command.rstrip()
+    try:
+        output = subporcess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except :
+        output = "Failed to execute command.\r\n"
+    return output
 
 def client_handler(client_socket):
-    pass
+    global upload
+    global execute
+    global command
+
+    if len(upload_destination):
+        file_buffer = ""
+
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+            try:
+                file_descriptor = open(upload_destination, "wb")
+                file_descriptor.write(file_buffer)
+                file_descriptor.close()
+                client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
+            except expression as identifier:
+                client_socket.send("Faild to save file to %s\r\n" % upload_destination)
+        
+        if len(execute):
+            output = run_command(execute)
+            client_socket.send(output)
+
+        if command:
+            while True:
+                client_socket.send("<NETCST:#>")
+                cmd_buffer = ""
+                while "\n" not in cmd_buffer:
+                    cmd_buffer = client_socket.recv(1024)
+                    response = run_command(cmd_buffer)
+                    client_socket.send(response)
 
 def main():
     global listen
@@ -73,6 +141,8 @@ def main():
             upload_destination = a
         elif o in ("-t", "--target"):
             target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
         else:
             assert False, "Unhandled Option"
 
