81d1ddb71860d7da4a5bdfb28ee0a75d92fb3537
trainking <yefulincode@gmail.com>
2021-01-10 15:11:01 +0800

    fix 队列控制

--- py/dos2unix.py
+++ py/dos2unix.py
@@ -8,7 +8,10 @@ import threading
 import queue
 import time
 
-workQueue = queue.Queue(10) # 数据队列
+MAX_THREADS = 4
+QUEUE_BUFFER = 10
+
+workQueue = queue.Queue(QUEUE_BUFFER) # 数据队列
 queueLock = threading.Lock() # 锁
 
 threads = [] # 线程集合
@@ -56,8 +59,9 @@ def main():
     Usage:
         python dos2uinx.py a.txt b.txt c.txt
     '''
+    script_name = os.path.basename(__file__)
     args = sys.argv[1:]
-    if args[0] == 'dos2unix.py':
+    if script_name in args[0]:
         args = args[1:]
 
     print("strat!")
@@ -67,13 +71,10 @@ def main():
         workQueue.put(x)
     queueLock.release()
 
-    for i in range(1, 3):
+    for i in range(0, MAX_THREADS):
         thread = SimpleThread(i, "thread-"+str(i), workQueue)
         thread.start()
         threads.append(thread)
-
-    while not workQueue.empty():
-        pass
 
     for t in threads:
         t.join()
